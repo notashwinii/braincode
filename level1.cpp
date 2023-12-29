a6b912eee0eeac70fc3405e9c3de41620583c941
ashwini <ashwinii.subedi@gmail.com>
2023-12-29 16:33:09 +0545

    .

--- level1.cpp
+++ level1.cpp
@@ -1,12 +1,18 @@
 #include "level1.h"
 #include "ui_level1.h"
+#include<QPixmap>
 
 level1::level1(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::level1)
 {
+    //constructor
     ui->setupUi(this);
+    QPixmap pix("D:/braincode/pictures/questions/raven_a1.jpg");
+    ui->label_a1->setPixmap(pix);
+
     Level2 = new level2();
+
 }
 
 level1::~level1()
