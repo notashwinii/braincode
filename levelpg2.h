27871f2e40ab5b23c9630b388bafcf74ed078f33
ashwini <ashwinii.subedi@gmail.com>
2023-12-30 21:33:40 +0545

    .

--- levelpg2.h
+++ levelpg2.h
@@ -2,19 +2,10 @@
 #define LEVELPG2_H
 
 #include <QMainWindow>
-#include"levelpg3.h"
 #include "level13.h"
 #include "level14.h"
 #include "level15.h"
-#include "level16.h"
-#include "level17.h"
-#include "level18.h"
-#include "level19.h"
-#include "level20.h"
-#include "level21.h"
-#include "level22.h"
-#include "level23.h"
-#include "level24.h"
+
 
 namespace Ui {
 class levelpg2;
@@ -41,39 +32,14 @@ private slots:
 
     void on_pushButton15_clicked();
 
-    void on_pushButton16_clicked();
-
-    void on_pushButton17_clicked();
-
-    void on_pushButton18_clicked();
-
-    void on_pushButton19_clicked();
-
-    void on_pushButton20_clicked();
-
-    void on_pushButton21_clicked();
-
-    void on_pushButton22_clicked();
-
-    void on_pushButton23_clicked();
-
-    void on_pushButton24_clicked();
 
 private:
     Ui::levelpg2 *ui;
-    levelpg3 *Levelpg3;
+
     level13 *Level13;
     level14 *Level14;
     level15 *Level15;
-    level16 *Level16;
-    level17 *Level17;
-    level18 *Level18;
-    level19 *Level19;
-    level20 *Level20;
-    level21 *Level21;
-    level22 *Level22;
-    level23 *Level23;
-    level24 *Level24;
+
 
 };
 
